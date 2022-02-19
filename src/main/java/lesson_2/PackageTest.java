b95bf0fef5d444c36df4ad538a26e228751b054e
aruytehno <aruytehno@gmail.com>
2022-02-20 01:25:19 +0300

    Added new examples

--- src/main/java/lesson_2/PackageTest.java
+++ src/main/java/lesson_2/PackageTest.java
@@ -1,11 +1,15 @@
 package lesson_2;
 
+import java.util.Arrays;
+
 public class PackageTest {
     public static void main(String[] args) {
         System.out.println("хаааай");
 
+        System.out.println("args: " + args);
+        System.out.println("Arrays.toString(args): " + Arrays.toString(args));
         for (String arg : args) {
-            System.out.println(arg);
+            System.out.println("\"" + arg + "\"");
         }
     }
 }
